d5becb6b9e6c68ee79a6d7924abb768ddf1f9b16
Denis Kurilenko <kurilenkodenis@gmail.com>
2018-10-24 18:18:10 +0300

    Remove hash generation in method Swu.HashToPoint, Add tests.

---
+++ Passw0rd.Tests/SwuTests.cs
@@ -0,0 +1,40 @@
+namespace Passw0rd.Tests
+{
+    using System;
+    using Org.BouncyCastle.Asn1.Nist;
+    using Org.BouncyCastle.Crypto.Digests;
+    using Org.BouncyCastle.Math.EC;
+    using Org.BouncyCastle.Security;
+ 
+    using Passw0rd.Phe;
+
+    using Xunit;
+
+    public class SwuTests
+    {
+        [Fact]
+        public void Should_GeneratePointOnCurve_When_RandomHashesArePassed()
+        {
+            var curveParams = NistNamedCurves.GetByName("P-256");
+
+            var swu = new Swu(((FpCurve)curveParams.Curve).Q, curveParams.Curve.B.ToBigInteger());
+            var rng = new SecureRandom();
+            var sha = new Sha512tDigest(256);
+            var random = new byte[32];
+
+            for (int i = 0; i <= 10000; i++)
+            {
+                var hash = new byte[32];
+
+                rng.NextBytes(random);
+                sha.BlockUpdate(random, 0, random.Length);
+                sha.DoFinal(hash, 0);
+
+                var (x, y) = swu.HashToPoint(hash);
+                Assert.True(curveParams.Curve.CreatePoint(x, y).IsValid());
+
+                sha.Reset();
+            }
+        }
+    }
+}
--- Passw0rd/PHE/Swu.cs
+++ Passw0rd/PHE/Swu.cs
@@ -1,10 +1,42 @@
-﻿namespace Passw0rd.PHE
-{
-    using System.Security.Cryptography;
+﻿/*
+ * Copyright (C) 2015-2018 Virgil Security Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *     (3) Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+ */
 
-    using Org.BouncyCastle.Asn1.X9;
+namespace Passw0rd.Phe
+{
     using Org.BouncyCastle.Math;
-    using Org.BouncyCastle.Math.EC;
 
     /// <summary>
     /// Implementation of Shallue-Woestijne-Ulas algorithm
@@ -18,64 +50,58 @@
         private readonly BigInteger p14;
         private readonly BigInteger mba;
 
-        public Swu()
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Swu"/> class.
+        /// </summary>
+        public Swu(BigInteger p, BigInteger b)
         {
-            var curve = (FpCurve)ECNamedCurveTable.GetByName("P-256").Curve;
-
-            this.p = curve.Q;
-            this.b = curve.B.ToBigInteger();
-
-            this.a = p.Neg(BigInteger.Three);
-            var ba = p.Div(this.b, a);
-            this.mba = p.Neg(ba);
-            var p3 = p.Sub(p, BigInteger.Three);
-            this.p34 = p.Div(p3, BigInteger.ValueOf(4));
-            var p1 = p.Add(p, BigInteger.One);
-            this.p14 = p.Div(p1, BigInteger.ValueOf(4));
+            this.p   = p; 
+            this.b   = b; 
+            this.a   = p.Neg(BigInteger.Three);
+            this.mba = p.Neg(p.Div(this.b, a));
+            this.p34 = p.Div(p.Sub(p, BigInteger.Three), BigInteger.ValueOf(4));
+            this.p14 = p.Div(p.Add(p, BigInteger.One), BigInteger.ValueOf(4));
         }
 
-        public (BigInteger x, BigInteger y) HashToPoint(byte[] data)
+        /// <summary>
+        /// Maps 32 byte hash to a point on curve.
+        /// </summary>
+        public (BigInteger x, BigInteger y) HashToPoint(byte[] hash)
         {
-            using (var sha256 = SHA256.Create())
-            {
-                var hash = sha256.ComputeHash(data);
+            var t = new BigInteger(1, hash, 0, hash.Length);
+            t = t.Mod(p);
 
-                var t = new BigInteger(1, hash, 0, hash.Length);
-                t = t.Mod(p);
+            var tt = p.Square(t);
+            var alpha = p.Neg(tt);
+            var asq = p.Square(alpha);
+            var asqa = p.Add(asq, alpha);
+            var asqa1 = p.Add(BigInteger.One, p.Inv(asqa));
 
-                var tt = p.Square(t);
-                var alpha = p.Neg(tt);
-                var asq = p.Square(alpha);
-                var asqa = p.Add(asq, alpha);
-                var asqa1 = p.Add(BigInteger.One, p.Inv(asqa));
+            // x2 = -(b / a) * (1 + 1/(alpha^2+alpha))
+            var x2 = p.Mul(mba, asqa1);
 
-                // x2 = -(b / a) * (1 + 1/(alpha^2+alpha))
-                var x2 = p.Mul(mba, asqa1);
+            // x3 = alpha * x2
+            var x3 = p.Mul(alpha, x2);
+            var ax2 = p.Mul(a, x2);
+            var x23 = p.Cube(x2);
+            var x23ax2 = p.Add(x23, ax2);
 
-                // x3 = alpha * x2
-                var x3 = p.Mul(alpha, x2);
-                var ax2 = p.Mul(a, x2);
-                var x23 = p.Cube(x2);
-                var x23ax2 = p.Add(x23, ax2);
+            // h2 = x2^3 + a*x2 + b
+            var h2 = p.Add(x23ax2, b);
+            var ax3 = p.Mul(a, x3);
+            var x33 = p.Cube(x3);
+            var x33ax3 = p.Add(x33, ax3);
+            var h3 = p.Add(x33ax3, b);
 
-                // h2 = x2^3 + a*x2 + b
-                var h2 = p.Add(x23ax2, b);
-                var ax3 = p.Mul(a, x3);
-                var x33 = p.Cube(x3);
-                var x33ax3 = p.Add(x33, ax3);
-                var h3 = p.Add(x33ax3, b);
+            // tmp = h2 ^ ((p - 3) // 4)
+            var tmp = p.Pow(h2, p34);
+            var tmp2 = p.Square(tmp);
+            var tmp2h2 = p.Mul(tmp2, h2);
 
-                // tmp = h2 ^ ((p - 3) // 4)
-                var tmp = p.Pow(h2, p34);
-                var tmp2 = p.Square(tmp);
-                var tmp2h2 = p.Mul(tmp2, h2);
-
-                // if tmp^2 * h2 == 1:
-
-                return tmp2h2.Equals(BigInteger.One)
-                    ? (x2, p.Mul(tmp, h2))
-                    : (x3, p.Pow(h3, p14));
-            }
+            // if tmp^2 * h2 == 1:
+            return tmp2h2.Equals(BigInteger.One)
+                ? (x2, p.Mul(tmp, h2))
+                : (x3, p.Pow(h3, p14));
         }
     }
 }
