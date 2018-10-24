db857dbf322cd3ac6fffcee499d56895acf7de1a
Denis Kurilenko <kurilenkodenis@gmail.com>
2018-10-24 18:54:07 +0300

    Develop a PHE crypto using Bouncy Castle crypto prymitives

---
+++ Passw0rd.Tests/PheCryptoTests.cs
@@ -0,0 +1,115 @@
+﻿namespace Passw0rd.Tests
+{
+    using System;
+
+    using Passw0rd.Phe;
+    using Passw0rd.Utils;
+
+    using Xunit;
+    using Xunit.Abstractions;
+
+    public class PheCryptoTests
+    {
+        private readonly ITestOutputHelper output;
+
+        public PheCryptoTests(ITestOutputHelper output)
+        {
+            this.output = output;
+        }
+
+        [Fact]
+        public void Should_ComputeIdenticalC0AndC1Values_When_TheSameKeyAndNonceArePassed()
+        {
+            var phe = new PheCrypto();
+
+            var skS = phe.DecodeSecretKey(Bytes.FromString("I4ETKFzr3QmUu+Olhp1L2KvRgjfseO530R/A+aQ80Go=", StringEncoding.BASE64));
+            var nS = Bytes.FromString("4g1N7hTxVWEHesoGZ5eTwbufdSnRtzxzkEQaBkXWsL4=", StringEncoding.BASE64);
+            var c0 = Bytes.FromString("BI4mieAp/rdVctneZnhj0Ucu8Sc4LGMu2P5z9j49iXtN3AhDBWgIS1A4kfLI1ktcQAJACK4vwgutomtuWSoYllc=", StringEncoding.BASE64);
+            var c1 = Bytes.FromString("BHENDFDcDsaWwpZLAWXDvEXlrEpIwr1p+OESiRCSemnk41WdfObVsvGPsYNFopaCJN762vP4MINb9HGzjmbM+aU=", StringEncoding.BASE64);
+
+            var (c00, c11) = phe.ComputeC(skS, nS);
+
+            Assert.Equal(c0, c00);
+            Assert.Equal(c1, c11);
+        }
+
+        [Fact]
+        public void Should_ComputeTheSameC0Value_For_SpecifiedListOfParameters()
+        {
+            var phe = new PheCrypto();
+
+            var pwd = Bytes.FromString("passw0rd");
+            var skC = phe.DecodeSecretKey(Bytes.FromString("gPdKsQRz9Vmc/DnbfxCHUioU6omEa0Sg7pncSHOhA7I=", StringEncoding.BASE64));
+            var nC = Bytes.FromString("CTnPcIn2xcz+4/9jjLuFZNJiWLBiohnaGVTb3X1zPt0=", StringEncoding.BASE64);
+            var c0 = Bytes.FromString("BKAH5Rww+a9+8lzO3oYE8zfz2Oxp3+Xrv2jp9EFGRlS7bWU5iYoWQHZqrkM+UYq2TYON3Gz8w3mzLSy3yS0XlJw=", StringEncoding.BASE64);
+            var t0 = Bytes.FromString("BPGwrvLl3CAolVo1RIoNT7TqTC3T66eLykxa/Vw1gyof+3scfaiqTAAQXQ57q6zEkEeJHjNl4GBghQceI/UNl7c=", StringEncoding.BASE64);
+
+            var c00 = phe.ComputeC0(skC, pwd, nC, t0);
+
+            Assert.Equal(c0, c00);
+        }
+
+        [Fact]
+        public void Should_ProveTheProofOfSuccess_For_SpecifiedListOfParameters()
+        {
+            var phe   = new PheCrypto();
+
+            var skS   = phe.DecodeSecretKey(Bytes.FromString("I4ETKFzr3QmUu+Olhp1L2KvRgjfseO530R/A+aQ80Go=", StringEncoding.BASE64));
+            var c0    = Bytes.FromString("BKAH5Rww+a9+8lzO3oYE8zfz2Oxp3+Xrv2jp9EFGRlS7bWU5iYoWQHZqrkM+UYq2TYON3Gz8w3mzLSy3yS0XlJw=", StringEncoding.BASE64);
+            var c1    = Bytes.FromString("BOkgpxdavc+CYqmeKboBPhEjgXCEKbb5HxFeJ1+rVoOm006Jbu5a/Ol4rK5bVbkhzGAT+gUZaodUsnEuucfIbJc=", StringEncoding.BASE64);
+
+            var term1 = Bytes.FromString("BJkkIassffljoINliNRNcR7J/PmYcAl9PPUBeIT2I6oBy6wKb5gZsIOchWEN5pNfLVPH1V0BuhhlTi7MZeBlPOY=", StringEncoding.BASE64);
+            var term2 = Bytes.FromString("BIiZfmmzuffwmJmibNnpws9D5SkgkPMBYxbgaWS1274kbJKC7WakUp1Mzk9BWvah0kDomJhqzyV7/8ZBX1rGX9I=", StringEncoding.BASE64);
+            var term3 = Bytes.FromString("BCBcx9GsjQcoKI0pW4t49WWS/Z1bmg62KlCjAzB1IsKVDYQAT4213UuIMBthNnxSOVUUHikZCUw01mX5XKGQD5A=", StringEncoding.BASE64);
+            var blndX = Bytes.FromString("KFiLnVVliWdm3fdYcuFK1sTiw1hvbKSesy4sGlYO8Rs=", StringEncoding.BASE64);
+
+            var skC = phe.DecodeSecretKey(Bytes.FromString("gPdKsQRz9Vmc/DnbfxCHUioU6omEa0Sg7pncSHOhA7I=", StringEncoding.BASE64));
+
+            var nS  = Bytes.FromString("POKVRG0nmZc9062v41TNFngibsgMKzt/BY6lZ/5pcZg=", StringEncoding.BASE64);
+
+            var isValid = phe.ValidateProofOfSuccess(skS.PublicKey, nS, c0, c1, term1, term2, term3, blndX);
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void Should_ProveTheProofOfFail_When_SpecifiedListOfParametersArePassed()
+        {
+            var phe = new PheCrypto();
+
+            var skS = phe.DecodeSecretKey(Bytes.FromString("I4ETKFzr3QmUu+Olhp1L2KvRgjfseO530R/A+aQ80Go=", StringEncoding.BASE64));
+            var c0 = Bytes.FromString("BKAH5Rww+a9+8lzO3oYE8zfz2Oxp3+Xrv2jp9EFGRlS7bWU5iYoWQHZqrkM+UYq2TYON3Gz8w3mzLSy3yS0XlJw=", StringEncoding.BASE64);
+            var c1 = Bytes.FromString("BOkgpxdavc+CYqmeKboBPhEjgXCEKbb5HxFeJ1+rVoOm006Jbu5a/Ol4rK5bVbkhzGAT+gUZaodUsnEuucfIbJc=", StringEncoding.BASE64);
+
+            var term1 = Bytes.FromString("BJkkIassffljoINliNRNcR7J/PmYcAl9PPUBeIT2I6oBy6wKb5gZsIOchWEN5pNfLVPH1V0BuhhlTi7MZeBlPOY=", StringEncoding.BASE64);
+            var term2 = Bytes.FromString("BIiZfmmzuffwmJmibNnpws9D5SkgkPMBYxbgaWS1274kbJKC7WakUp1Mzk9BWvah0kDomJhqzyV7/8ZBX1rGX9I=", StringEncoding.BASE64);
+            var term3 = Bytes.FromString("BCBcx9GsjQcoKI0pW4t49WWS/Z1bmg62KlCjAzB1IsKVDYQAT4213UuIMBthNnxSOVUUHikZCUw01mX5XKGQD5A=", StringEncoding.BASE64);
+            var blndX = Bytes.FromString("KFiLnVVliWdm3fdYcuFK1sTiw1hvbKSesy4sGlYO8Rs=", StringEncoding.BASE64);
+
+            var skC = phe.DecodeSecretKey(Bytes.FromString("gPdKsQRz9Vmc/DnbfxCHUioU6omEa0Sg7pncSHOhA7I=", StringEncoding.BASE64));
+
+            var nS = Bytes.FromString("POKVRG0nmZc9062v41TNFngibsgMKzt/BY6lZ/5pcZg=", StringEncoding.BASE64);
+
+            var isValid = phe.ValidateProofOfSuccess(skS.PublicKey, nS, c0, c1, term1, term2, term3, blndX);
+
+            Assert.True(isValid);
+        }
+
+        [Fact]
+        public void Should_DecryptM_When_SpecifiedListOfParametersArePassed()
+        {
+            var phe = new PheCrypto();
+
+            var pwd = Bytes.FromString("passw0rd");
+            var skC = phe.DecodeSecretKey(Bytes.FromString("Ty+3kM7kPFQjZu1NKyJjHqdUnDl9/n7mlHu7VJMyLRw=", StringEncoding.BASE64));
+            var nC  = Bytes.FromString("9Gr0Jrg+vux+xBOLpGLXemB7uuwq7xkIa4JVgAJG5Yw=", StringEncoding.BASE64);
+            var c1  = Bytes.FromString("BMkSH2xpOLox46HQf0oq8vqtYApMezo7K83xCMDgsUzgpI75esc6KJTRrqf7Nq5+y2LiGwyfJ/X8wZ7IXywy+gg=", StringEncoding.BASE64);
+            var t1  = Bytes.FromString("BPGSx7b84QaTFJoXJcQ70qPuppbGkh8udqSIZJ7R8AE6Br0CBvPw69exsa3jxeHmMN17vY9l9wXdZcQt7FST2fc=", StringEncoding.BASE64);
+            var m   = Bytes.FromString("XslZXIrsVj1XYVF74gWfi0Lo4frLsDObirNl5mFwYeA=", StringEncoding.BASE64);
+
+            var mm  = phe.DecryptM(skC, pwd, nC, t1, c1);
+
+            Assert.Equal(m, mm);
+        }
+    }
+}
---
+++ Passw0rd/Phe/PheCrypto.cs
@@ -0,0 +1,338 @@
+﻿/*
+ * Copyright (C) 2015-2018 Virgil Security Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *     (3) Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+ */
+
+namespace Passw0rd.Phe
+{
+    using System;
+    using System.Text;
+
+    using Passw0rd.Utils;
+
+    using Org.BouncyCastle.Asn1.Nist;
+    using Org.BouncyCastle.Asn1.X9;
+    using Org.BouncyCastle.Crypto.Digests;
+    using Org.BouncyCastle.Crypto.Generators;
+    using Org.BouncyCastle.Crypto.Parameters;
+    using Org.BouncyCastle.Math;
+    using Org.BouncyCastle.Math.EC;
+    using Org.BouncyCastle.Math.EC.Multiplier;
+    using Org.BouncyCastle.Security;
+
+    /// <summary>
+    /// Phe crypto.
+    /// </summary>
+    public class PheCrypto
+    {
+        private X9ECParameters curveParams;
+        private FpCurve curve;
+        private SecureRandom rng;
+        private TupleHash tupleHash;
+        private Swu swu;
+        private MixedNafR2LMultiplier multiplier;
+
+        private byte[] dhc0 = Encoding.UTF8.GetBytes("hc0");
+        private byte[] dhc1 = Encoding.UTF8.GetBytes("hc1");
+        private byte[] dm   = Encoding.UTF8.GetBytes("dm");
+        private byte[] dhs0 = Encoding.UTF8.GetBytes("hs0");
+        private byte[] dhs1 = Encoding.UTF8.GetBytes("hs1");
+        private byte[] proofOK = Bytes.FromString("ProofOk");
+
+        public PheCrypto()
+        {
+            this.curveParams = NistNamedCurves.GetByName("P-256");
+            this.curve = (FpCurve)curveParams.Curve;
+            this.rng = new SecureRandom();
+            this.tupleHash = new TupleHash();
+            this.swu = new Swu(curve.Q, curve.B.ToBigInteger()); ;
+            this.multiplier = new MixedNafR2LMultiplier();
+        }
+
+        /// <summary>
+        /// Generates a random nonce.
+        /// </summary>
+        public byte[] GenerateNonce()
+        {
+            var nonce = new byte[32];
+            this.rng.NextBytes(nonce);
+
+            return nonce;
+        }
+
+        /// <summary>
+        /// Generates a new random secret key.
+        /// </summary>
+        public SecretKey GenerateSecretKey()
+        {
+            var randomZ = this.RandomZ();
+            var point   = this.multiplier.Multiply(this.curveParams.G, randomZ);
+
+            return new SecretKey(randomZ, (FpPoint)point);
+        }
+
+        /// <summary>
+        /// Decodes <see cref="SecretKey"/> from specified byte array.
+        /// </summary>
+        public SecretKey DecodeSecretKey(byte[] encodedSecretKey)
+        {
+            var secretKeyInt = new BigInteger(1, encodedSecretKey);
+
+            Console.WriteLine(secretKeyInt);
+            var point = this.multiplier.Multiply(this.curveParams.G, secretKeyInt);
+
+            return new SecretKey(secretKeyInt, (FpPoint)point);
+        }
+
+        /// <summary>
+        /// Decodes <see cref="PublicKey"/> from specified byte array.
+        /// </summary>
+        public PublicKey DecodePublicKey(byte[] encodedPublicKey)
+        {
+            var point = (FpPoint)this.curve.DecodePoint(encodedPublicKey);
+            return new PublicKey(point);
+        }
+
+        /// <summary>
+        /// Computes the record T for specified password.
+        /// </summary>
+        public Tuple<byte[], byte[]> ComputeT(SecretKey skC, byte[] pwd, byte[] nC, byte[] c0, byte[] c1)
+        {
+            // TODO: validation
+            var c0Point  = this.curve.DecodePoint(c0);
+            var c1Point  = this.curve.DecodePoint(c1);
+
+            var mbuf = new byte[32];
+            this.rng.NextBytes(mbuf);
+
+            var mPoint   = this.HashToPoint(dm, mbuf);
+            var hc0Point = this.HashToPoint(dhc0, nC, pwd);
+            var hc1Point = this.HashToPoint(dhc1, nC, pwd);
+            var t0Point  = c0Point.Add(this.multiplier.Multiply(hc0Point, skC.Value));
+            var t1Point  = c1Point.Add(this.multiplier.Multiply(hc1Point, skC.Value)
+                .Add(this.multiplier.Multiply(mPoint, skC.Value)));
+
+            return new Tuple<byte[], byte[]>(t0Point.GetEncoded(), t1Point.GetEncoded());
+        }
+
+        /// <summary>
+        /// Computes the c.
+        /// </summary>
+        public Tuple<byte[], byte[]> ComputeC(SecretKey skS, byte[] nS)
+        {
+            var hs0 = this.HashToPoint(dhs0, nS);
+            var hs1 = this.HashToPoint(dhs1, nS);
+
+            var c0 = this.multiplier.Multiply(hs0, skS.Value);
+            var c1 = this.multiplier.Multiply(hs1, skS.Value);
+
+            return new Tuple<byte[], byte[]>(c0.GetEncoded(), c1.GetEncoded());
+        }
+
+        /// <summary>
+        /// Decrypts the M value for specified password.
+        /// </summary>
+        public byte[] DecryptM(SecretKey skC, byte[] pwd, byte[] nC, byte[] t1, byte[] c1)
+        {
+            var t1Point  = this.curve.DecodePoint(t1);
+            var c1Point  = this.curve.DecodePoint(c1);
+            var hc1Point = this.HashToPoint(dhc1, nC, pwd);
+            var minusY   = this.curveParams.N.Neg(skC.Value);
+
+            var mPoint = this.multiplier.Multiply(t1Point.Add(c1Point.Negate())
+                .Add(this.multiplier.Multiply(hc1Point, minusY)), skC.Value.ModInverse(this.curveParams.N));
+
+            var hkdf = new HkdfBytesGenerator(new Sha512tDigest(256));
+            hkdf.Init(new HkdfParameters(mPoint.GetEncoded(), null, Encoding.UTF8.GetBytes("Secret")));
+            var key = new byte[32];
+            hkdf.GenerateBytes(key, 0, key.Length);
+
+            return key;
+        }
+
+        /// <summary>
+        /// Computes the c0 point for specified T record and password.
+        /// </summary>
+        public byte[] ComputeC0(SecretKey skC, byte[] pwd, byte[] nc, byte[] t0)
+        {
+            var hc0Point = this.HashToPoint(dhc0, nc, pwd);
+            var minusY = skC.Value.Negate();
+
+            var t0Point = this.curve.DecodePoint(t0);
+            var c0Point = t0Point.Add(this.multiplier.Multiply(hc0Point, minusY));
+
+            return c0Point.GetEncoded();
+        }
+
+        /// <summary>
+        /// Updates an encryption record T with the specified update token parameters.
+        /// </summary>
+        public Tuple<byte[], byte[]> UpdateT(byte[] nS, byte[] t0, byte[] t1, byte[] a, byte[] b)
+        {
+            var hs0Point = this.HashToPoint(dhs0, nS);
+            var hs1Point = this.HashToPoint(dhs1, nS);
+
+            var t0Point = this.curve.DecodePoint(t0);
+            var t1Point = this.curve.DecodePoint(t1);
+
+            var aInt = new BigInteger(1, a);
+            var bInt = new BigInteger(1, b);
+
+            var t00Point = this.multiplier.Multiply(t0Point, aInt).Add(this.multiplier.Multiply(hs0Point, bInt));
+            var t11Point = this.multiplier.Multiply(t1Point, aInt).Add(this.multiplier.Multiply(hs1Point, bInt));
+
+            return new Tuple<byte[], byte[]>(t00Point.GetEncoded(), t11Point.GetEncoded());
+        }
+
+        /// <summary>
+        /// Proves the success.
+        /// </summary>
+        public Tuple<byte[], byte[], byte[], byte[]> ProveSuccess(SecretKey skS, byte[] nS, byte[] c0, byte[] c1)
+        {
+            var blindX    = this.RandomZ();
+            var hs0Point  = this.HashToPoint(dhs0, nS);
+            var hs1Point  = this.HashToPoint(dhs1, nS);
+            var term1     = this.multiplier.Multiply(hs0Point, blindX).GetEncoded();
+            var term2     = this.multiplier.Multiply(hs1Point, blindX).GetEncoded();
+            var term3     = this.multiplier.Multiply(this.curveParams.G, blindX).GetEncoded();
+            var pubKey    = skS.PublicKey.Encode();
+            var curveG    = this.multiplier.Multiply(this.curveParams.G, BigInteger.ValueOf(1));
+            var challenge = this.HashZ(proofOK, pubKey, curveG.GetEncoded(), c0, c1, term1, term2, term3);
+            var result    = blindX.Add(skS.Value.Multiply(challenge)).ToByteArray();
+
+            return new Tuple<byte[], byte[], byte[], byte[]>(term1, term2, term3, result);
+        }
+
+        public bool ValidateProofOfSuccess(PublicKey pkS, byte[] nS, byte[] c0, byte[] c1, byte[] term1, byte[] term2, byte[] term3, byte[] blindX)
+        {
+            var trm1Point = this.curve.DecodePoint(term1);
+            var trm2Point = this.curve.DecodePoint(term2);
+            var trm3Point = this.curve.DecodePoint(term3);
+            var blindXInt = new BigInteger(1, blindX);
+
+            var c0Point   = this.curve.DecodePoint(c0);
+            var c1Point   = this.curve.DecodePoint(c1);
+
+            var hs0Point  = this.HashToPoint(dhs0, nS);
+            var hs1Point  = this.HashToPoint(dhs1, nS);
+
+            var curveG    = this.multiplier.Multiply(this.curveParams.G, BigInteger.ValueOf(1));
+            var challenge = this.HashZ(proofOK, pkS.Encode(), curveG.GetEncoded(), c0, c1, term1, term2, term3);
+
+            var t1Point   = trm1Point.Add(this.multiplier.Multiply(c0Point, challenge));
+            var t2Point   = this.multiplier.Multiply(hs0Point, blindXInt);
+
+            if (!t1Point.Equals(t2Point)) 
+            {
+                return false;
+            }
+
+            t1Point = trm2Point.Add(this.multiplier.Multiply(c1Point, challenge));
+            t2Point = this.multiplier.Multiply(hs1Point, blindXInt);
+
+            if (!t1Point.Equals(t2Point))
+            {
+                return false;
+            }
+
+            t1Point = trm3Point.Add(this.multiplier.Multiply(pkS.Point, challenge));
+            t2Point = this.multiplier.Multiply(this.curveParams.G, blindXInt);
+
+            if (!t1Point.Equals(t2Point))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Generates big random 256 bit integer which must be less than 
+        /// curve's N parameter.
+        /// </summary>
+        private BigInteger RandomZ()
+        {
+            BigInteger z;
+            byte[] zBytes = new byte[32];
+
+            do
+            {
+                this.rng.NextBytes(zBytes);
+                z = new BigInteger(1, zBytes);
+            }
+            while (z.CompareTo(this.curveParams.N) >= 0);
+
+            return z;
+        }
+
+        /// <summary>
+        /// Maps arrays of bytes to an integer less than curve's N parameter
+        /// </summary>
+        private BigInteger HashZ(byte[] domain, params byte[][] datas)
+        {
+            var hash = this.tupleHash.Sum(domain, datas);
+
+            Console.WriteLine("Th  : " + Bytes.ToString(hash, StringEncoding.BASE64));
+
+            var hkdf = new HkdfBytesGenerator(new Sha512tDigest(256));
+            hkdf.Init(new HkdfParameters(hash, domain, Encoding.UTF8.GetBytes("TupleKDF")));
+            var result = new byte[32];
+
+            BigInteger z;
+
+            do
+            {
+                hkdf.GenerateBytes(result, 0, result.Length);
+                z = new BigInteger(1, result);
+            }
+            while (z.CompareTo(this.curveParams.N) >= 0);
+
+            return z;
+        }
+
+        /// <summary>
+        /// Maps arrays of bytes to a valid curve point.
+        /// </summary>
+        private FpPoint HashToPoint(byte[] domain, params byte[][] datas)
+        {
+            var hash   = this.tupleHash.Sum(domain, datas);
+            var (x, y) = this.swu.HashToPoint(hash);
+
+            var xField = this.curve.FromBigInteger(x);
+            var yField = this.curve.FromBigInteger(y);
+
+            return (FpPoint)this.curve.CreatePoint(x, y);
+        }
+    }
+}
