b80b3ba598a57bd18932c57d454b4dad4771ad64
Denis Kurilenko <kurilenkodenis@gmail.com>
2018-10-24 18:49:02 +0300

    Add secret and public keys classes

---
+++ Passw0rd/Phe/SecretKey.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Copyright (C) 2015-2018 Virgil Security Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ *     (1) Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *     (2) Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in
+ *     the documentation and/or other materials provided with the
+ *     distribution.
+ *
+ *     (3) Neither the name of the copyright holder nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Lead Maintainer: Virgil Security Inc. <support@virgilsecurity.com>
+ */
+
+namespace Passw0rd.Phe
+{
+    using Org.BouncyCastle.Math;
+    using Org.BouncyCastle.Math.EC;
+
+    public class SecretKey
+    {
+        internal SecretKey(BigInteger bigInt, FpPoint point)
+        {
+            this.Value = bigInt;
+            this.PublicKey = new PublicKey(point);
+        }
+
+        internal BigInteger Value { get; private set; }
+
+        /// <summary>
+        /// Gets the public key.
+        /// </summary>
+        public PublicKey PublicKey { get; private set; }
+
+        public byte[] Encode()
+        {
+            return this.Value.ToByteArrayUnsigned(); 
+        }
+    }
+}
